2764ace3f078d21d905922d00fcfbebe06daa2e1
Bilaal <bilaalsadiq@gmail.com>
2024-10-30 15:10:06 +0000

    Add endpoint to get all employees

---
+++ EmployeeAdminPortal/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var result = _dbContext.Employees.ToList();
+            return Ok(result);
+        }
+    }
+}
